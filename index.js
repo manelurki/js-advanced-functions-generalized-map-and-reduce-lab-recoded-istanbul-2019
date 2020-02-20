21f4ed1601060b35f6d859949a90dc7ed5c19490
manel turki <menelturki@gmail.com>
2020-02-20 08:42:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-function map(src) {
+function map(src, cb) {
   let r = []
 
   for (let i = 0; i < src.length; i++) {
     let theElement = src[i]
-    r.push(theElement)
+    r.push(cb(theElement))
   }
 
   return r;
 }
 
-function reduce(src, starting){
+function reduce(src, cb, starting){
   let r = (!!starting) ? starting : src[0]
   let i = (!!starting) ? 0 : 1
 
   for (; i < src.length; i++) {
-    r = (src[i], r)
+    r = cb(src[i], r)
   }
 
   return r;
