fb4aa7e9c644b7bba9f9d07496cf67f58b965367
manel turki <menelturki@gmail.com>
2020-02-20 08:36:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,15 @@ function reduce(arr, fn, startingValue = 0) {
     }
     return sum
 }
+function reduce(arr, fn, startingPoint) {
+    let val = arr[0];
+    let start = 1;
+    if (startingPoint !== undefined) {
+        val = startingPoint;
+        start = 0;
+    }
+    for (let i = start; i < arr.length; i++) {
+        val = fn(val, arr[i]);
+    }
+    return val;
+}
