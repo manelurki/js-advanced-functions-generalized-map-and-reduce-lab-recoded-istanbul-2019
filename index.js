a5757f83decb28391a44b8f2eb6efc0a98d941da
manel turki <menelturki@gmail.com>
2020-02-20 08:41:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,50 +1,21 @@
-function waldo(array) {
-    let result = []
-    let column;
-    let row;
-    let unique = ""
-    let obj = {}
-    let newArr = []
-    array.forEach(element => {
-        newArr.push(element)
-    })
-​
-​
-    newArr = newArr.join().split(',')
-​
-    newArr.forEach(element => {
-        if (!obj[element]) {
-            obj[element] = 1
-        } else { obj[element] += 1 }
-    })
-​
-    for (const key in obj) {
-​
-        if (obj.hasOwnProperty(key)) {
-            if (obj[key] === 1) {
-                unique = key
-​
-            }
-        }
-    }
-​
-    array.forEach(element => {
-​
-        if (element.includes(unique)) {
-​
-            column = array.indexOf(element) + 1
-        }
-​
-        for (let index = 0; index < element.length; index++) {
-            if (element[index] === unique) {
-                row = index + 1
-            }
-​
-        }
-    })
-​
-    result.push(column)
-    result.push(row)
-​
-    return result
+function map(src) {
+  let r = []
+
+  for (let i = 0; i < src.length; i++) {
+    let theElement = src[i]
+    r.push(theElement)
+  }
+
+  return r;
+}
+
+function reduce(src, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = (src[i], r)
+  }
+
+  return r;
 }
