f18bb8d11080e79d90ca595c0bbeda70793e5aee
manel turki <menelturki@gmail.com>
2020-02-20 08:33:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+function reduce(array, fn, start = null) {
+    let total
+    if (start == null) {
+        total = array[0]
+        for (let index = 1; index < array.length; index++) {
+            total = fn(total, array[index], array)
+        }
+        return total;
+    } else {
+        total = start
+        for (let index = 0; index < array.length; index++) {
+            total = fn(total, array[index], array)
+        }
+        return total;
+    }
+}
+function reduce(arr, fn, startingValue = 0) {
+    let sum = 0
+    let memo = 0
+    for (let i = 1; i < arr.length; i++) {
+        memo += arr[i - 1]
+        sum = fn(arr[i], memo + startingValue)
+    }
+    return sum
+}
