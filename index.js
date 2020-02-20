fc717d03f18e4354eb7402c40f9e5798974a9156
manel turki <menelturki@gmail.com>
2020-02-20 08:44:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+function map(arr, fn) {
+  let r = []
+
+  for (let i = 0; i < arr.length; i++) {
+    let theElement = arr[i]
+    r.push(fn(theElement))
+  }
+
+  return r;
+}
+function reduce(arr, fn, startingPoint) {
+    let val = arr[0];
+    let start = 1;
+    if (startingPoint !== undefined) {
+        val = startingPoint;
+        start = 0;
+    }
+    for (let i = start; i < arr.length; i++) {
+        val = fn(val, arr[i]);
+    }
+    return val;
+}
